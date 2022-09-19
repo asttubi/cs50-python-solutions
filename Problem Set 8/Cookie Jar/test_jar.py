46de6e912c48d0e2b65c78838cfd0951d705b514
Tuba <76249160+asttubi@users.noreply.github.com>
2022-09-19 18:20:34 +0300

    Create test_jar.py

---
+++ Problem Set 8/Cookie Jar/test_jar.py
@@ -0,0 +1,32 @@
+from jar import Jar
+import pytest
+
+def test_init():
+    jar=Jar()
+    assert jar.capacity==12
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    jar=Jar()
+    jar.deposit(3)
+    assert jar.size==3
+
+    with pytest.raises (ValueError):
+        jar.deposit(15)
+
+def test_withdraw():
+    jar=Jar()
+    jar.deposit(5)
+    jar.withdraw(3)
+    assert jar.size==2
+
+    with pytest.raises(ValueError):
+        jar.withdraw(5)>jar.size(4)
